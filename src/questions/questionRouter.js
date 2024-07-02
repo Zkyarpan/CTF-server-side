edc55ece15d6422108efaf72768b45efe26414db
Zkyarpan <karkiarpan555@gmail.com>
2024-07-02 09:53:39 +0545

    FIx getAlluser controller

--- src/questions/questionRouter.js
+++ src/questions/questionRouter.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { isAdmin, authenticateToken } = require("../middlewares/authHandle");
+const {
+  isAdmin,
+  authenticateToken,
+  isUser,
+} = require("../middlewares/authHandle");
 
 const {
   createQuestionSet,
@@ -17,7 +21,6 @@ questionRouter.post("/create", authenticateToken, isAdmin, createQuestionSet);
 questionRouter.get(
   "/getAllQuestion",
   authenticateToken,
-  isAdmin,
   getAllQuestion
 );
 
